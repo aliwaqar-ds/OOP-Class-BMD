0247d87ae5d1b89e74ccc40899722304b7c75bf8
Ali Waqar <ali.waqar0328@gmail.com>
2026-09-22 21:07:23 +0500

    Add files via upload

---
+++ runner.java
@@ -0,0 +1,39 @@
+//class Rectangle{
+//    int lenght, width;
+//    public int calculateArea(){
+//        return(lenght * width);
+//    }
+//}
+//
+//public class runner{
+//    public static void main(String[] args) {
+//        Rectangle rect  = new Rectangle();
+//        rect.lenght = 10;
+//        rect.width = 10;
+//        System.out.println(rect.calculateArea());
+//    }
+//}
+
+//class Rectangle {
+//    public int length, width;
+//    public Rectangle() {
+//        length = 5;
+//        width = 2;
+//    }
+//    public Rectangle(int l, int w) {
+//        length = l;
+//        width = w;
+//    }
+//    public int Calculatearea() {
+//        return (length * width);
+//    }
+//}
+//
+//public class runner {
+//    public static void main(String args[]) {
+//        Rectangle rect = new Rectangle();
+//        System.out.println(rect.Calculatearea());
+//        Rectangle rect1 = new Rectangle(10,20);
+//        System.out.println(rect1.Calculatearea());
+//    }
+//}
